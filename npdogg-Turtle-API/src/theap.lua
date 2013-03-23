a98d58d7e64fe5c70bcee6ab76b0505db16cb89d
Nigel <Nigel@10.203.4.182>
2013-03-23 04:03:47 -0600

    Implemented delete for binary heap implementation.
    Signed-off-by: Nigel <Nigel@10.203.4.182>

--- npdogg-Turtle-API/src/theap.lua
+++ npdogg-Turtle-API/src/theap.lua
@@ -6,19 +6,32 @@ function BinaryHeap.new()
 	
 	local heap = {}
 	local values = {}
+	local positions = {}
 	local base = 0
 	
-	local function swap(parent_index, child_index)
-		local swap = heap[parent_index]
-		heap[parent_index] = heap[child_index]
-		heap[child_index] = swap
+	local function getLeftChildIndex(index)
+		return index * 2
+	end
+	
+	local function getRightChildIndex(index)
+		return index * 2 + 1
+	end
+	
+	local function getParentIndex(index)
+		return math.floor(index / 2)
+	end
+	
+	local function swap(a, b)
+		local swap = heap[a]
+		heap[a] = heap[b]
+		heap[b] = swap
 	end
 	
 	local function bubble(index)
 		local value = values[heap[index]]
 		
 		while index > 1 do
-			local parent_index = math.floor(index / 2)
+			local parent_index = getParentIndex(index)
 			local parent_value = values[heap[parent_index]]
 			
 			if value <= parent_value then
@@ -28,41 +41,67 @@ function BinaryHeap.new()
 				break
 			end
 		end
+		
+		return index
 	end
 	
-	local function index(key)
-		for index, stored_key in ipairs(heap) do
-			if stored_key == key then
-				return index
+	local function sift(index)
+		local value = values[heap[index]]
+		local left_child_index = getLeftChildIndex(index)
+		local right_child_index = getRightChildIndex(index)
+		local left_child_value = values[heap[left_child_index]]
+		local right_child_value = values[heap[right_child_index]]
+		local min_child_index = index
+		local min_child_value
+		
+		if right_child_value == nil then
+			if left_child_value ~= nil then
+				min_child_index = left_child_index
 			end
+		else
+			min_child_index = (left_child_value <= right_child_value) and left_child_index or right_child_index
 		end
+				
+		min_child_value = values[heap[min_child_index]]
+		
+		if value > min_child_value then
+			swap(index, min_child_index)
+			index = sift(min_child_index)
+		end
+		
+		return index
 	end
 	
-	function this.size()
-		return base
+	local function deleteMin()
+		local key = heap[1]
+		values[key] = nil
+		heap[1] = heap[base]
+		key = heap[1]
+		heap[base] = nil
+		base = base - 1
+		positions[key] = sift(1)
 	end
 	
 	function this.min()
-		return heap[1]
-	end
-	
-	function this.value(key)
-		return values[key]
+		local min = heap[1]
+		deleteMin()
+		
+		return min
 	end
 	
 	function this.insert(key, value)
 		base = base + 1
 		values[key] = value
 		heap[base] = key
-		bubble(base)
+		positions[key] = bubble(base)
 	end
 	
 	function this.update(key, value)
-		local index = index(key)
+		local index = positions[key]
 		values[key] = value
-		bubble(index)
+		positions[key] = bubble(index)
 	end
-		
+
 	return this
 
 end
