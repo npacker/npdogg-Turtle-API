541e546e5f59825b0a589f4749de6ee14bfec6ce
Nigel <Nigel@10.203.27.85>
2013-03-24 00:13:51 -0600

    Testing code does not need to be under version control at this point.
    Signed-off-by: Nigel <Nigel@10.203.27.85>

--- npdogg-Turtle-API/test/test.lua
+++ npdogg-Turtle-API/test/test.lua
@@ -1,23 +1,18 @@
 require "theap"
-require "tposition"
+require "tvector"
 require "tmap"
 require "tpath"
 
 local world = Map.new()
-world.fill(Position.new(1, 0, 0))
-world.fill(Position.new(0, -1, 0))
-world.fill(Position.new(0, 1, 0))
-world.fill(Position.new(-1, 0, 0))
-world.fill(Position.new(0, 0, -1))
-world.fill(Position.new(4, 0, 1))
-local start = Position.new(0, 0, 0)
-local goal = Position.new(16, 256, 16)
+world.fill(Vector.new(1, 0, 0))
+local start = Vector.new(0, 0, 0)
+local goal = Vector.new(64, 64, 64)
 local start_time = os.clock()
 local path = a_star(start, goal, world)
 local end_time = os.clock()
 	
-for _, step in ipairs(path) do
-	print(step.toString())
+for _, waypoint in ipairs(path) do
+	print(waypoint.toString())
 end
 
 print("\n" .. end_time - start_time)
